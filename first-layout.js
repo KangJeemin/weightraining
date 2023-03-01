6eed4ba1629f64eff1d225ee355284a5dc64edce
jiminKang <kang@jiminKangs-MacBook-Pro.local>
2023-03-01 18:53:20 +0900

    첫 화면 레이아웃 완성  (글꼴 및 색 수정 필요)

--- first-layout.js
+++ first-layout.js
@@ -6,23 +6,55 @@ const MainLayoutHeightFlex = [1,2,1];
 
 
 for(let i=0;i<3;i++){
-const a = document.createElement('div');
-root.appendChild(a);
-root.children[i].style.width = `100%`;
-root.children[i].style.flex = MainLayoutHeightFlex[i];
+    const a = document.createElement('div');
+    root.appendChild(a);
+    root.children[i].style.width = `100%`;
+    root.children[i].style.flex = MainLayoutHeightFlex[i];
 }
-// 첫번쨰 화면 시간과 로고 있는 컨테이너
+// 첫번쨰 화면 시간과 로고 있는 컨테이너-----------------------------------------
 root.children[0].style.display="flex";
 root.children[0].style.flexDirection="column";
 
 for(let i =0; i<2;i++){
-const div = document.createElement('div');
-root.children[0].appendChild(div);
-root.children[0].children[i].style.width=`100%`;
+    const div = document.createElement('div');
+    root.children[0].appendChild(div);
+    root.children[0].children[i].style.width=`100%`;
 }
+root.children[0].children[0].style.justifyContent="center"
+root.children[0].children[0].style.display="flex";
 root.children[0].children[0].style.flex=1;
 root.children[0].children[1].style.flex=5;
 
+function getTime(){
+    const date = new Date();
+    let hour = date.getHours();
+    let mintues = date.getMinutes();
+    let second = date.getSeconds();
+    let time = document.getElementById('time');  
+    root.children[0].children[0].innerHTML=`${hour} : ${mintues} : ${second} `;
+}
+function timeSet() {
+    getTime()
+    setInterval(getTime,1000)
+}
+  
+window.onload = timeSet();
+
+// 첫번째 화면 아레나로고 컨테이너------------------------------------------------------
+root.children[1].style.display="flex";
+root.children[1].style.justifyContent="center";
+// root.children[1].style.fontFamily="Roboto";
+root.children[1].innerHTML="ARENA";
+
+// 첫번째 화면 운동 시작 컨테이너----------------------------------------------------
+
+const button = document.createElement('button');
+root.children[2].appendChild(button);
+root.children[2].style.display="flex";
+root.children[2].style.justifyContent="center";
+root.children[2].children[0].innerHTML="운동시작";
+root.children[2].children[0].style.width=`25%`
+root.children[2].children[0].style.height=`25%`
 
 
 
@@ -33,4 +65,5 @@ function rootSize(width,height) {
 
 rootSize(390,844);
 console.dir(root);
-
+console.dir(root.children[2]);
+getTime();
